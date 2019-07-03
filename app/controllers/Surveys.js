f2de249bce57fcb8bc643bb8b207fcca04fbe0f1
Aakil Fernandes <aakilfernandes@gmail.com>
2019-07-03 14:20:29 -0400

    survey link and highlighting

--- app/controllers/Surveys.js
+++ app/controllers/Surveys.js
@@ -3,6 +3,21 @@ import authManager from '../authManager.js'
 
 export default async function SurveysController($scope) {
 
+  let highlightedSurveyId
+
+  async function setHighlightedSurvey() {
+    const hashParts = window.location.hash.split('/')
+    if (hashParts[1] !== 'polls') {
+      return
+    }
+    const surveyId = parseInt(hashParts[2])
+    if (!surveyId) {
+      return
+    }
+
+    highlightedSurveyId = surveyId
+  }
+
   async function refresh() {
     $scope.isUpdating = true
     $scope.me = await authManager.fetchMe()
@@ -21,11 +36,26 @@ export default async function SurveysController($scope) {
       })
     }
 
+    if (highlightedSurveyId) {
+      const highlightedSurvey = $scope.surveys.find((survey) => {
+        return survey.id === highlightedSurveyId
+      })
+      if (highlightedSurvey) {
+        highlightedSurvey.isHighlighted = true
+        setTimeout(() => {
+          const $highlightedSurvey = document.querySelectorAll(`[survey-id="${highlightedSurveyId}"]`)[0]
+          $highlightedSurvey.scrollIntoView()
+        })
+
+      } else {
+        highlightedSurvey.isHighlighted = false
+      }
+    }
+
+
     $scope.isUpdating = false
     $scope.$apply()
   }
-
-  refresh()
 
   $scope.vote = async function setVote(surveyId, answerId) {
     $scope.isUpdating = true
@@ -37,4 +67,8 @@ export default async function SurveysController($scope) {
 
     await refresh()
   }
+
+  setHighlightedSurvey()
+  refresh()
+
 }
